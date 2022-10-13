574d689ee16b63727fd446111462ae844f616890
mohmmed23 <mhmmedben3@gmail.com>
2022-10-13 10:38:47 +0300

    fixed name fun in build file and edit name biuld scrept #8

--- src/db/build.ts
+++ src/db/build.ts
@@ -1,5 +1,12 @@
 import sequelize from './index'
-const a = async () => {
-  await sequelize.sync({ force: true })
+import { User, Post, Like, Animal, Tag } from '../models'
+import data from './data.json'
+import environment from '../config/environment'
+
+if (environment.nodeEnv !== 'test') {
+  const buildDB = async () => {
+    await sequelize.sync({ force: true })
+  }
+
+  buildDB().then(res => console.log(res)).catch(err => console.log(err))
 }
-a()
