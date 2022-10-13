2a34e4f22c2d65b0657edbb098f632ce6eb8ead2
mohmmed23 <mhmmedben3@gmail.com>
2022-10-13 10:52:34 +0300

    fixed #8

--- src/db/build.ts
+++ src/db/build.ts
@@ -3,10 +3,10 @@ import { User, Post, Like, Animal, Tag } from '../models'
 import data from './data.json'
 import environment from '../config/environment'
 
-if (environment.nodeEnv !== 'test') {
-  const buildDB = async () => {
-    await sequelize.sync({ force: true })
-  }
+const buildDB = async () => {
+  await sequelize.sync({ force: true })
+}
 
+if (environment.nodeEnv !== 'test') {
   buildDB().then(res => console.log(res)).catch(err => console.log(err))
 }
