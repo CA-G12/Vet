0f34367e663b5fa781d86f3eed796c1961e2f48f
aaamra <dev.aaamra@gmail.com>
2022-10-13 15:24:03 +0300

    fix

--- src/db/build.ts
+++ src/db/build.ts
@@ -1,13 +1,18 @@
-import sequelize from './index'
-import { User, Post, Like, Animal, Tag } from '.'
+import { User, Post, Like, Animal, Tag, sequelize } from '.'
 import data from './seeds.json'
 import environment from '../config/environment'
 
 const buildDB = async () => {
   await sequelize.sync({ force: true })
   await User.bulkCreate(data.User)
+  await Post.bulkCreate(data.Post)
+  await Like.bulkCreate(data.Like)
+  await Animal.bulkCreate(data.Animal)
+  await Tag.bulkCreate(data.Tag)
 }
 
 if (environment.nodeEnv !== 'test') {
-  buildDB().then(res => console.log(res)).catch(err => console.log(err))
+  buildDB()
 }
+
+export default buildDB
