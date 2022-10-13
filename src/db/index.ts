e218b482c8add4a7e027fa15ce04b111e6772133
mohmmed23 <mhmmedben3@gmail.com>
2022-10-13 10:09:00 +0300

    add index db

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,23 +1,23 @@
 import { User, Like, Comment, Animal, Post, Booking, Tag } from '../models'
 import sequelize from './connection'
 
-User.hasMany(Post, { foreignKey: 'userId' })
+User.hasMany(Post)
 Post.belongsTo(User)
 
-User.hasMany(Like, { foreignKey: 'userId' })
+User.hasMany(Like)
 Like.belongsTo(User)
 
 User.hasMany(Comment)
 Comment.belongsTo(User)
 
 User.hasMany(Booking, { as: 'bookings' })
-Booking.belongsTo(User, { as: 'user', foreignKey: 'userId' })
-Booking.belongsTo(User, { as: 'doctor', foreignKey: 'doctorId' })
+Booking.belongsTo(User, { as: 'user' })
+Booking.belongsTo(User, { as: 'doctor' })
 
 Post.hasMany(Comment)
 
 Comment.belongsTo(Post)
-Post.hasMany(Like, { foreignKey: 'postId' })
+Post.hasMany(Like)
 Like.belongsTo(Post)
 
 Tag.hasMany(Post)
