8ab2928254bc042e50ff2dbb7bd25e0964b4a5c3
salsabeelomar <omarsalsabeel65@gmail.com>
2022-10-13 15:14:24 +0300

    make forign keys pascal case relates #10

--- src/db/build.ts
+++ src/db/build.ts
@@ -1,10 +1,11 @@
 import sequelize from './index'
-import { User, Post, Like, Animal, Tag } from '../models'
-import data from './data.json'
+import { User, Post, Like, Animal, Tag } from '.'
+import data from './seeds.json'
 import environment from '../config/environment'
 
 const buildDB = async () => {
   await sequelize.sync({ force: true })
+  await User.bulkCreate(data.User)
 }
 
 if (environment.nodeEnv !== 'test') {
