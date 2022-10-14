4e600cdc90e27c64ff603ba6127c4f86d57dc3a8
mohmmed23 <mhmmedben3@gmail.com>
2022-10-14 12:15:25 +0300

    add tests and fixed bug in getPostFile #14

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -9,11 +9,61 @@ beforeEach(() => build())
 describe('all post ', () => {
   test('all post', (done) => {
     supertest(app)
-      .get('/post')
+      .get('/posts')
       .expect('Content-Type', /json/)
       .end((err, res) => {
         if (err) return done(err)
         expect(res.body.length).toEqual(2)
+        return done()
+      })
+  })
+})
+describe('get posts by specific animal ', () => {
+  test('get posts by specific animal', (done) => {
+    supertest(app)
+      .get('/posts?animalId=1')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.length).toEqual(1)
+        return done()
+      })
+  })
+})
+
+describe('get posts by specific tag ', () => {
+  test('get posts by specific tag', (done) => {
+    supertest(app)
+      .get('/posts?tagId=1')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.length).toEqual(1)
+        return done()
+      })
+  })
+})
+
+describe('get some post when add searchCountent ,tag,animal ', () => {
+  test('get some post when add searchCountent ,tag,animal', (done) => {
+    supertest(app)
+      .get('/posts?tagId=1&animalId=1&searchCountent=1111')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.length).toEqual(1)
+        return done()
+      })
+  })
+})
+describe('Not getting any data when When searching for something that is not there ', () => {
+  test('Not getting any data when When searching for something that is not there', (done) => {
+    supertest(app)
+      .get('/posts?tagId=1&animalId=1&searchCountent=kakashi')
+      .expect('Content-Type', /json/)
+      .end((err, res) => {
+        if (err) return done(err)
+        expect(res.body.length).toEqual(0)
         return done()
       })
   })
--- src/controllers/GetPosts.ts
+++ src/controllers/GetPosts.ts
@@ -3,12 +3,14 @@ import { User, Post, Like, Tag, Animal } from '../db'
 import { Op } from 'sequelize'
 
 const getPosts = async (req: Request, res: Response) => {
-  const { tagId, animalId, searchContent } = req.query
+  const { tagId, animalId, searchCountent } = req.query
+  console.log(req.query)
+
   let filterData = { }
   tagId && animalId
     ? filterData = {
       content: {
-        [Op.substring]: searchContent
+        [Op.substring]: searchCountent || ''
       },
       TagId: tagId,
       AnimalId: animalId
@@ -16,22 +18,31 @@ const getPosts = async (req: Request, res: Response) => {
     : animalId
       ? filterData = {
         content: {
-          [Op.substring]: searchContent
+          [Op.substring]: searchCountent || ''
         },
         AnimalId: animalId
       }
       : tagId
         ? filterData = {
           content: {
-            [Op.substring]: searchContent
+            [Op.substring]: searchCountent || ''
           },
           TagId: tagId
         }
+
         : filterData = {
           content: {
-            [Op.substring]: searchContent
+            [Op.substring]: ''
           }
         }
+  console.log(filterData)
+
+  // : searchContent
+  // ? filterData = {
+  //   content: {
+  //     [Op.substring]: searchContent
+  //   }
+  // }
   const posts = await Post.findAll({
     attributes: ['id', 'content', 'image'],
     include: [{ model: User, attributes: ['name', 'avatar', 'id'] },
@@ -42,6 +53,6 @@ const getPosts = async (req: Request, res: Response) => {
       }],
     where: filterData
   })
-  res.json(JSON.parse(JSON.stringify(posts)))
+  res.json((posts))
 }
 export default getPosts
