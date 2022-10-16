a075250e1061c38e86bd104f334ae41c129eb7d8
Saeed99Madi <saed.zooro@gmail.com>
2022-10-16 15:00:52 +0300

    make configration for sign in operation related to #13

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express'
 import compression from 'compression'
 import cookieParser from 'cookie-parser'
 import environment from './config/environment'
+import passport from 'passport'
+import './config/auth'
 import { join } from 'path'
 class App {
   public app: Application
@@ -19,6 +21,7 @@ class App {
     this.app.use(cookieParser())
     this.app.use(express.urlencoded({ extended: false }))
     this.app.use(express.static(join(__dirname, '..', 'client', 'build')))
+    this.app.use(passport.initialize())
   }
 }
 
