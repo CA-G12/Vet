f34532022a283a279edd6fcb610f9155c689a1fc
Saeed99Madi <saed.zooro@gmail.com>
2022-10-16 15:09:47 +0300

    make tests in sign in routes feature related to #13

---
+++ __Tests__/signIn.test.ts
@@ -0,0 +1,63 @@
+import { afterAll, beforeAll, describe, test } from '@jest/globals'
+import router from '../src/app'
+import supertest from 'supertest'
+import dbBuild from '../src/db/build'
+import sequelize from '../src/db/connection'
+
+beforeAll(() => dbBuild())
+
+describe('sign in router', () => {
+  test('incorrect password', (done) => {
+    supertest(router)
+      .post('/signin')
+      .send({
+        email: 'saeed@gmail.com',
+        password: '123asa456'
+      })
+      .expect(422)
+      .expect('Content-Type', /html/)
+      .end((err:any, res: any) => {
+        console.log(err, 'hello her1')
+        if (err) { done(err) } else {
+          return done()
+        }
+      })
+  })
+
+  test('email is not exist', (done) => {
+    supertest(router)
+      .post('/signin')
+      .send({
+        email: 'most0717883@gmail.com',
+        password: '123456'
+      })
+      .expect(422)
+      .expect('Content-Type', /html/)
+      .end((err:any, res: any) => {
+        console.log(err, 'hello her1')
+
+        if (err) done(err)
+        else {
+          return done()
+        }
+      })
+  })
+  test('check if the user logged in successfully', (done) => {
+    supertest(router)
+      .post('/signin')
+      .send({
+        email: 'saeed@gmail.com',
+        password: '123456'
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end((err:any, res: any) => {
+        if (err) done(err)
+        else {
+          return done()
+        }
+      })
+  })
+})
+
+afterAll(() => sequelize.close())
---
+++ src/routes/users.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express'
+import UsersController from '../controllers/authController'
+import passport from 'passport'
+
+const signInRouter = Router()
+
+signInRouter.get('/auth/google',
+  passport.authenticate('google',
+    {
+      scope: ['email', 'profile'], session: false
+    }))
+
+signInRouter.get('/oauth2/redirect/google',
+  passport.authenticate('google', { failureRedirect: '/auth/google/fail', session: false }),
+  UsersController.googleAuth)
+
+signInRouter.get('/auth/google/failure', (req, res) => {
+  res.render('/auth/fail')
+})
+
+signInRouter.post('/signin', UsersController.signin)
+signInRouter.get('/', (req, res) => {
+  res.send('hello')
+})
+
+export default signInRouter
