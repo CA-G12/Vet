2a96747b1aa80653b386281df140d2ef8e42d7d8
Saeed99Madi <saed.zooro@gmail.com>
2022-10-17 23:09:53 +0300

    edit CustomError Calss related to #28

--- src/helpers/errorsHandling/CustumError.ts
+++ src/helpers/errorsHandling/CustumError.ts
@@ -1,11 +1,8 @@
 class CustumError extends Error {
   status:number
-  message:string
-
   constructor (status:number, message:string) {
     super(message)
     this.status = status
-    this.message = message
   }
 }
 
