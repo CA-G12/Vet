590c3e40faf6a6587e90f9621bfda336390ffc2f
Saeed99Madi <saed.zooro@gmail.com>
2022-10-19 17:28:18 +0300

    solve conflict related to #13

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-<<<<<<< HEAD
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
-=======
-  transform: { '^.+\\.ts?$': 'ts-jest' },
->>>>>>> 46c04420113d6b74b03b9aaa13d85605f798cb7c
   transformIgnorePatterns: ['<rootDir>/node_modules/']
 }
