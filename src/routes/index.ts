35bb082280c9ffc5cc289ee6191b108faf09de21
Saeed99Madi <saed.zooro@gmail.com>
2022-10-23 11:48:14 +0300

    splve routes conflict related to #13

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,17 @@
 import { Router } from 'express'
-<<<<<<< HEAD
+
 import signInRouter from './users'
 import PostsController from '../controllers/PostsController'
 
-const router = Router()
-router.get('/posts', PostsController.index)
-router.use(signInRouter)
-=======
-import PostRoutes from './posts'
 import auth from './auth'
+import PostRoutes from './posts'
 
 const router = Router()
+
+router.get('/posts', PostsController.index)
+
+router.use(signInRouter)
 router.use(PostRoutes)
 router.use(auth)
->>>>>>> 4b8cb7aa7cee5d45b8fcda921785fff31bc46f50
 
 export default router
