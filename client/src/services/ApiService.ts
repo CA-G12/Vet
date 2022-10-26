d1dda1e7ac7786fa05e27bfb3363affd151587a1
Saeed99Madi <saed.zooro@gmail.com>
2022-10-26 12:00:52 +0300

    solve conflict related to #30

--- client/src/services/ApiService.ts
+++ client/src/services/ApiService.ts
@@ -1,9 +1,8 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosInstance } from 'axios';
 
 import JwtService from './JwtService';
 
 export default class ApiServices {
-<<<<<<< HEAD
   private static axios: AxiosInstance | null;
 
   public static init():void {
@@ -15,18 +14,6 @@ export default class ApiServices {
         'Content-Type': 'application/json',
       },
     });
-=======
-  private static axios = axios;
-
-  public static init():void {
-    this.axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
-  }
-
-  public static setHeader() {
-    this.axios.defaults.headers.common.Authorization = `Bearer ${JwtService.getToken()}`;
-    this.axios.defaults.headers.common.Accept = 'application/json';
-    this.axios.defaults.headers.common['Content-Type'] = 'application/json';
->>>>>>> 67f9c495aa881b936470eddf7e47c3d357884c6f
   }
 
   public static get(endPoint:string, config?:AxiosRequestConfig):Promise<AxiosResponse> {
