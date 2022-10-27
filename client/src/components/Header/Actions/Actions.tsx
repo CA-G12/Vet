9f245ca88f151e30adb8d9c42ba6b9285d1aca08
Saeed99Madi <saed.zooro@gmail.com>
2022-10-27 15:15:10 +0300

    make actions Field in Header functional related to #30

--- client/src/components/Header/Actions/Actions.tsx
+++ client/src/components/Header/Actions/Actions.tsx
@@ -1,4 +1,9 @@
+import { useContext, useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
+
+import { useLocation } from 'react-router-dom';
+import { authContext } from '../../../hooks/useAuth';
+
 import { FireCall } from './FireCall/FireCall';
 import { Search } from './Search/Search';
 
@@ -6,18 +11,45 @@ import { UserTicket } from './UserTicket/UserTicket';
 import { AuthButtons } from './AuthButtons/AuthButtons';
 
 import ActionsStyles from './ActionsStyles';
+// import IAuth from '../../../Interfaces/IAuth';
 
 export const Actions = () => {
-  const fireCall = true;
-  const search = true;
-  const userTicket = false;
-  const authButtons = true;
+  const { user } = useContext(authContext);
+  const [isAuth, setIsAuth] = useState(false);
+  const [isLandingPage, setIsLandingPage] = useState(false);
+  const [isAdminDashboard, setIsAdminDashboard] = useState(true);
+  const location = useLocation();
+
+  // handle user isAuth
+  useEffect(() => {
+    if (user) {
+      setIsAuth(true);
+    } else {
+      setIsAuth(false);
+    }
+  }, [user]);
+  // handle route pathName
+  useEffect(() => {
+    // condition to change isLandingPage
+    if (location.pathname === '/') {
+      setIsLandingPage(true);
+    } else {
+      setIsLandingPage(true);
+    }
+    // condition to change isAdminDashboard
+    if (location.pathname === '/admin/dashboard') {
+      setIsAdminDashboard(false);
+    } else {
+      setIsAdminDashboard(true);
+    }
+  }, [location]);
+
   return (
     <Box sx={ActionsStyles.actionsStyle}>
-      {search ? <Search /> : null}
-      {fireCall ? <FireCall /> : null}
-      {userTicket ? <UserTicket /> : null}
-      {authButtons ? <AuthButtons /> : null}
+      {isLandingPage ? <Search /> : null}
+      {isAdminDashboard ? <FireCall /> : null}
+      {isAuth ? <UserTicket user={user} /> : null}
+      {!isAuth ? <AuthButtons /> : null}
     </Box>
   );
 };
