da2caab7f27f2f8564c052a948f29df479333e3a
Saeed99Madi <saed.zooro@gmail.com>
2022-10-27 15:20:42 +0300

    make userTicket Changable depending to Auth User related to #30

--- client/src/components/Header/Actions/UserTicket/UserTicket.tsx
+++ client/src/components/Header/Actions/UserTicket/UserTicket.tsx
@@ -1,15 +1,18 @@
-// @flow
-// import React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import userTicketStyles from './UserTicketStyle';
+import IAuth from '../../../../Interfaces/IAuth';
 
-export const UserTicket = () => (
+type props = {
+  user:IAuth | null | undefined
+}
+
+export const UserTicket = ({ user }:props) => (
   <Box sx={userTicketStyles.actionsStyle}>
-    <Avatar sx={userTicketStyles.avatar} alt="Remy Sharp" src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80" />
+    <Avatar sx={userTicketStyles.avatar} alt="Remy Sharp" src={user?.avatar} />
     <Typography sx={userTicketStyles.username} gutterBottom variant="h5" component="div">
-      Saeid
+      {user?.name}
     </Typography>
   </Box>
 );
