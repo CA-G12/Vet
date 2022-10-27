e637a2968a841bd490b18eb476b28bd52e70ca39
Saeed99Madi <saed.zooro@gmail.com>
2022-10-27 15:13:48 +0300

    put header in Application related to #30

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,15 @@ import { ToastContainer } from 'react-toastify';
 import Popup from './components/Popup/Popup';
 import 'react-toastify/dist/ReactToastify.css';
 import PostsContext from './Context/PostsContext';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import { ProvideAuth } from './hooks/useAuth';
+import { Header } from './components/Header/Header';
 
 const App = () => (
   <ProvideAuth>
     <ToastContainer />
     <PostsContext>
-      <Nav />
+      <Header />
       <Popup />
       <Outlet />
     </PostsContext>
