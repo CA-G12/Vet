e8b8642150b1a4b0ca5491c3203a3d21a2255c3b
AseelL <aseel.sahmood@gmail.com>
2022-11-01 22:32:48 +0200

    added initial update in backend

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,8 +1,10 @@
 import PostsController from '../controllers/PostsController';
 import { Router } from 'express';
+import { isAuth } from '../middlewares/auth.middleware';
 
 const router = Router();
 router.post('/posts', PostsController.store);
 router.get('/posts', PostsController.index);
+router.put('/posts', isAuth, PostsController.update);
 
 export default router;
