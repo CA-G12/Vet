60ae420f8527e122ef54629f5cc7f90c2e476102
Saeed99Madi <saed.zooro@gmail.com>
2022-11-02 22:55:27 +0200

    make routes protected related to #81

--- src/routes/appointment.ts
+++ src/routes/appointment.ts
@@ -1,20 +1,39 @@
 import Appointment from '../controllers/Appointment';
 import express from 'express';
 import errorWrapper from '../helpers/errorsHandling/customErrorWrapper';
+import { isAuth, isRole } from '../middlewares/auth.middleware';
 
 const router = express.Router();
-router.get('/Appointment/:id', errorWrapper(Appointment.doctorAppointment));
+router.get(
+  '/Appointment/:id',
+  errorWrapper(isAuth),
+  errorWrapper(Appointment.doctorAppointment),
+);
 router.delete(
   '/Appointment/:doctorId/:id',
+  errorWrapper(isAuth),
   errorWrapper(Appointment.deleteDoctorAppointment),
 );
-router.post('/Appointment', errorWrapper(Appointment.insertDoctorAppointment));
-router.put('/Appointment', errorWrapper(Appointment.updateDoctorAppointment));
+router.post(
+  '/Appointment',
+  errorWrapper(isAuth),
+  errorWrapper(Appointment.insertDoctorAppointment),
+);
+router.put(
+  '/Appointment',
+  errorWrapper(isRole('DOCTOR')),
+  errorWrapper(Appointment.updateDoctorAppointment),
+);
 
 router.get(
   '/pending-appointment/:DoctorId',
+  errorWrapper(isRole('DOCTOR')),
   errorWrapper(Appointment.pendingAppointment),
 );
-router.put('/pending-appointment', errorWrapper(Appointment.acceptAppointment));
+router.put(
+  '/pending-appointment',
+  errorWrapper(isRole('DOCTOR')),
+  errorWrapper(Appointment.acceptAppointment),
+);
 
 export default router;
