3d5026e2cb5b56efc1dc2ce32302ddea417bdf69
AseelL <aseel.sahmood@gmail.com>
2022-11-03 08:51:22 +0200

    finished delete route tests

--- __tests__/deletePost.test.ts
+++ __tests__/deletePost.test.ts
@@ -3,6 +3,7 @@ import app from '../src/app';
 import supertest from 'supertest';
 import build from '../src/db/build';
 import sequelize from '../src/db/connection';
+import { User } from '../src/models';
 let token = '';
 beforeAll(() => build());
 beforeEach(async () => {
@@ -21,6 +22,28 @@ describe('delete a post', () => {
         if (err) return done(err);
         expect(200);
         expect(res.body.msg).toBe('post deleted successfully');
+        return done();
+      });
+  });
+  test('delete a post successfully', done => {
+    supertest(app)
+      .delete('/api/v1/post/1')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(400);
+        expect(res.body.msg).toBe('you cannot delete this post');
+        return done();
+      });
+  });
+  test("delete a post that doesn't exist, should be rejected", done => {
+    supertest(app)
+      .delete('/api/v1/post/20')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(400);
+        expect(res.body.msg).toBe('you cannot delete this post');
         return done();
       });
   });
