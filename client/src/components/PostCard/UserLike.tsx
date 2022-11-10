9b86e20691267b8668e78e4abaaa9888b3f7c3ca
dev-mohmmed-alshorafa <mohmmedalshorafa1996@gmail.com>
2022-11-10 08:44:23 +0200

    -edit icons color -add tooltip to show like icon -center posts in mobile screen

--- client/src/components/PostCard/UserLike.tsx
+++ client/src/components/PostCard/UserLike.tsx
@@ -12,12 +12,7 @@ const UserLike = ({
   avatar: string;
 }) => (
   <NavLink style={{ textDecoration: 'none', width: 0 }} to={`/users/${id}`}>
-    <Stack
-      marginTop="10px"
-      justifyContent="space-between"
-      alignItems="center"
-      direction="row"
-    >
+    <Stack marginTop="10px" alignItems="center" direction="row">
       <Avatar sx={{ width: 24, height: 24 }} alt={name} src={avatar} />
       <span style={{ alignSelf: 'center', paddingLeft: '10px' }}>{name}</span>
     </Stack>
