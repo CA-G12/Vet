0834c813e9a8a083898e140c6da3e2317e90c3e2
Saeed99Madi <saed.zooro@gmail.com>
2022-11-19 14:21:06 +0200

    eslint configration

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,12 +5,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
