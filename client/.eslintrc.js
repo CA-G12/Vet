0887cb0f826a0d6bbd7bdaad345c9324b1915473
Saeed99Madi <saed.zooro@gmail.com>
2022-11-19 14:31:15 +0200

    deploy configration

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,7 +5,14 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/jsx-runtime',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
